4031546f4eae4fa3ef00250bb689ebc32312897d
Timothy <lemayiant4@gmail.com>
2022-10-20 15:18:35 +0300

    completed assignment

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+    def bark
+        puts "Woof!"
+    end
+    def sit 
+        puts "The Dog is sitting"
+    end    
+end
+
+figo = Dog.new
+figo.bark
+
+snoopy = Dog.new
+snoopy.bark
